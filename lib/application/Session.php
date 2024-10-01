3ec854bf648570c7d27c55d79dec9714daac84c3
Rastislav Madera <maderarasto@gmail.com>
2024-10-02 00:09:38 +0200

    Adds a session static class to manipulate with session data.

---
+++ lib/application/Session.php
@@ -0,0 +1,47 @@
+<?php
+
+session_start();
+
+class Session
+{
+    public static function get(string $key, mixed $default = null)
+    {
+        if (!isset($_SESSION[$key])) {
+            return $default;
+        }
+
+        return $_SESSION[$key];
+    }
+
+    public static function keys()
+    {
+        return array_keys($_SESSION);
+    }
+
+    public static function all()
+    {
+        return $_SESSION;
+    }
+
+    public static function set(string $key, mixed $value)
+    {
+        $_SESSION[$key] = $value;
+    }
+
+    public static function clear(string|null $key = null)
+    {
+        if (isset($key)) {
+            unset($_SESSION[$key]);
+            return;
+        }
+
+        foreach (self::keys() as $key) {
+            unset($_SESSION[$key]);
+        }
+    }
+
+    public static function destroy()
+    {
+        session_destroy();
+    }
+}
